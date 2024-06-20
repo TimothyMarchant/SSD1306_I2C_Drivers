63d947397c4bd8f145ead372073e05c8c75502da
Timothy M <124601612+TimothyMarchant@users.noreply.github.com>
2024-06-20 10:02:48 -0400

    Update I2C_Drivers.h
    Corrected the header file.  The original one was for a different PIC where the I2C on the hardware is different.

--- Header_Files/I2C_Drivers.h
+++ Header_Files/I2C_Drivers.h
@@ -1,11 +1,10 @@
 //I2C drivers
 #ifndef I2C_Drivers_h
 #define I2C_Drivers_h
-void I2C_Start(void);
-void I2C_Restart(void);
+void I2C_Init(void);
+void I2C_Start(unsigned char address,unsigned char numberofbytes,unsigned char firstbyte);
 void I2C_Stop(void);
 unsigned char I2C_Write(unsigned char);
-void I2C_ACK(void);
-void I2C_Nack(void);
 unsigned char I2C_Read(void);
+void updateI2CCNT(unsigned char update);
 #endif
